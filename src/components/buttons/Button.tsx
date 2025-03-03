7ce1ed6ab3d3fc71650cfe229100f9d0894296cc
LazarevSlava <549132w@gmail.com>
2025-03-03 20:26:13 +0200

    second

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -3,10 +3,15 @@ import { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  customClass?: string;
 }
 
-function Button({ children }: ButtonProps) {
-  return <button className={style.button}>{children}</button>;
+function Button({ children, customClass = '', ...props }: ButtonProps) {
+  return (
+    <button className={`${style.button} ${style[customClass]}`} {...props}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,11 +1,17 @@
 import style from './Footer.module.scss';
 import Button from '../components/buttons/Button';
+import { useState } from 'react';
 
 export function Footer() {
+  const [isCompleted, setIsCompleted] = useState(true);
+
   return (
     <footer>
       <div className={style.footerBlock}>
-        <Button>{'Next Step'}</Button>
+        <span className={style.goBack}>Go Back</span>
+        <Button customClass={isCompleted ? 'confirm' : ''}>
+          {isCompleted ? 'Confirm' : 'Next Step'}
+        </Button>
       </div>
     </footer>
   );
