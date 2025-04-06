6d439441e48adf107ed19adfab4b1b51d4eb71ac
LazarevSlava <549132w@gmail.com>
2025-04-06 09:08:22 +0300

    some changes

--- src/pages/Step5.tsx
+++ src/pages/Step5.tsx
@@ -1,17 +1,13 @@
-import style from './Step5.module.scss';
 import ThankYouIcon from '../assets/images/icon-thank-you.svg?react';
-
+import StepContainer from './StepContainer';
 const Step5 = () => {
   return (
-    <div className={style.container}>
-      <ThankYouIcon className={style.icon} />
-      <h2 className={style.title}>Thank you!</h2>
-      <p className={style.description}>
-        Thanks for confirming your subscription! We hope you have fun using our
-        platform. If you ever need support, please feel free to email us at
-        support@loremgaming.com.
-      </p>
-    </div>
+    <StepContainer
+      icon={<ThankYouIcon />}
+      centerContent
+      title="Finishing up"
+      description="Double-check everything looks OK before confirming."
+    />
   );
 };
 
