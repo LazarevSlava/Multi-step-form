0f89e5996cd630ce286c907ab77b35a74cefd02e
LazarevSlava <549132w@gmail.com>
2025-04-06 10:35:36 +0300

    name project

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  base: '/multi-step-form/',
+  base: '/Multi-step-form/',
   plugins: [react(), svgr()],
 });
